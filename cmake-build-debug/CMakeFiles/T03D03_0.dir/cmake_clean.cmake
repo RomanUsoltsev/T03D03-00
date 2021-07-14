99ad0c9e2b86a4db286848d00ee7a22df671df03
Tashia Buttercup <tbutterc@cl-b7.nsk.21-school.ru>
2021-07-14 17:32:49 +0700

    null_test

---
+++ cmake-build-debug/CMakeFiles/T03D03_0.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/T03D03_0.dir/src/main.c.o"
+  "T03D03_0"
+  "T03D03_0.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/T03D03_0.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
